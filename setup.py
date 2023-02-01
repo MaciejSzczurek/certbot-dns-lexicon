c832e14dc3a5ea12592fb28ee43bfc882e149bbf
Maciej Szczurek <maciej.szczurek2@gmail.com>
2023-02-01 21:56:51 +0100

    Bumped beautifulsoup4 to v4.11.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "certbot==2.2.0",
         "PyYAML==5.4.1",
         "dns-lexicon==3.11.7",
-        "beautifulsoup4==4.11.1",
+        "beautifulsoup4==4.11.2",
         "requests==2.28.2",
     ],
     classifiers=[
