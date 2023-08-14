dfdfd302a7f9f9e6fdfc77bd8d53a514e6803f40
Maciej Szczurek <maciej.szczurek2@gmail.com>
2023-08-14 12:08:03 +0200

    Upgrade version to v1.0.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="certbot-dns-lexicon",
-    version="1.0.17",
+    version="1.0.18",
     author="Maciej Szczurek",
     python_requires=">=3.10",
     packages=find_packages(exclude=["lexicon", "lexicon.*"]),
