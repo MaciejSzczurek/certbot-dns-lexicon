0652ef5d5a9874d1c89276a5e7a3c21357422165
Maciej Szczurek <maciej.szczurek2@gmail.com>
2024-05-29 18:18:22 +0200

    Set version to v1.0.36

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="certbot-dns-lexicon",
-    version="1.0.35",
+    version="1.0.36",
     author="Maciej Szczurek",
     python_requires=">=3.10",
     packages=find_packages(exclude=["lexicon", "lexicon.*"]),
