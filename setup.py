d4cbb7dc854e3f05c4d262f8df9842687faa165e
Maciej Szczurek <maciej.szczurek2@gmail.com>
2024-12-20 10:48:04 +0100

    Upgrade dns-lexicon to v3.20.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         "certbot==3.0.1",
         "PyYAML==6.0.2",
-        "dns-lexicon==3.20.0",
+        "dns-lexicon==3.20.1",
         "beautifulsoup4==4.12.3",
         "requests==2.32.3",
     ],
