c30d3768054412b0b8f6cbf9bdb286cad8b1e0e6
Maciej Szczurek <maciej.szczurek2@gmail.com>
2025-02-03 09:58:02 +0100

    Upgrade Beautiful Soup to v4.13.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "certbot==3.1.0",
         "PyYAML==6.0.2",
         "dns-lexicon==3.20.1",
-        "beautifulsoup4==4.12.3",
+        "beautifulsoup4==4.13.0",
         "requests==2.32.3",
     ],
     classifiers=[
