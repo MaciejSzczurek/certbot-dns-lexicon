d859e931b8eb6dd0f712bfc0de2038d88e3780cc
Maciej Szczurek <maciej.szczurek2@gmail.com>
2025-02-12 10:46:15 +0100

    Upgrade certbot to v3.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     python_requires=">=3.10",
     packages=find_packages(exclude=["lexicon", "lexicon.*"]),
     install_requires=[
-        "certbot==3.1.0",
+        "certbot==3.2.0",
         "PyYAML==6.0.2",
         "dns-lexicon==3.20.1",
         "beautifulsoup4==4.13.3",
