c9761d9a6a29d74c9dc5e70379170e9341acfd7b
Maciej Szczurek <maciej.szczurek2@gmail.com>
2025-10-01 07:15:34 +0200

    Upgrade Beautiful Soup to v4.14.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "certbot==5.0.0",
         "PyYAML==6.0.3",
         "dns-lexicon==3.21.1",
-        "beautifulsoup4==4.14.0",
+        "beautifulsoup4==4.14.2",
         "requests==2.32.5",
     ],
     classifiers=[
